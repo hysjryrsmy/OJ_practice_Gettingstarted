97351b62b69c954562f32815fdd8ab78d1db72ef
hysjryrsmy <78720177+hysjryrsmy@users.noreply.github.com>
2021-06-24 11:04:26 +0900

    oj-397：僵尸来袭

---
+++ test397.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARING
+
+#include <iostream>
+using namespace std;
+
+
+int n, m, mmap[105][105], ans;
+int dir[4][2] = { 0,1,1,0,0,-1,-1,0 };
+
+void func(int x0, int y0) {
+	for (int i = 0; i < 4; i++) {
+		int x = x0 + dir[i][0];
+		int y = y0 + dir[i][1];
+		if (mmap[x][y] != 0) {
+			mmap[x][y] = 0;
+			func(x, y);
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> mmap[i][j];
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (mmap[i][j] != 0) {
+				ans++;
+				mmap[i][j] = 0;
+				func(i, j);
+			}
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
